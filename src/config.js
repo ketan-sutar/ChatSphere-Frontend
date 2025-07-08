dda58c3c3eb035ef2f1dc816fa3ad3323da39470
ketan-sutar <sutarketan35@gmail.com>
2025-07-08 21:39:23 +0530

    added axios link

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://chat-sphere-backend-lake.vercel.app";
+// export const BASE_URL = "https://chat-sphere-backend-lake.vercel.app/api/v1";
